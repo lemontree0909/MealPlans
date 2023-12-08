edd32b1630dc16a9e68ba437784df17bd48b51b8
lemontree0909 <lemontree0909@protonmail.com>
2023-12-08 13:17:09 +0300

    without localeStorage

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import uuid from 'react-uuid';
 
 function App() {
   const [mealPlans, setMealPlans] = useState(
-    localStorage.mealPlans ? JSON.parse(localStorage.mealPlans) : []);
+    // localStorage.mealPlans ? JSON.parse(localStorage.mealPlans) : []);
   const [selectedDay,setSelectedDay] = useState(false);
 
-  useEffect(() => {
-    localStorage.setItem("mealPlans", JSON.stringify(mealPlans))
-  }, [mealPlans])
+  // useEffect(() => {
+  //   localStorage.setItem("mealPlans", JSON.stringify(mealPlans))
+  // }, [mealPlans])
 
   const addMeal = () => {
     console.log("IT WORKS - ADDING")
